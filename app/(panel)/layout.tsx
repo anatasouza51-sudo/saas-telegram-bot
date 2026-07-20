a26fff5183adff8a30dcbc632845e192672f092a
anatasouza51 <anatasouza51@gmail.com>
2026-07-20 14:24:38 +0000

    debug: restore TopNavBar in layout to test

--- app/(panel)/layout.tsx
+++ app/(panel)/layout.tsx
@@ -1,5 +1,6 @@
 import { requireUser } from "@/lib/session"
 import type { ReactNode } from "react"
+import { TopNavBar } from "@/components/top-nav-bar"
 
 export const dynamic = "force-dynamic"
 
@@ -12,7 +13,7 @@ export default async function PanelLayout({
 
   return (
     <div>
-      <p>User: {user.name} ({user.email})</p>
+      <TopNavBar user={user} />
       {children}
     </div>
   )
